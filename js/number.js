544c61fc4edccefc57407f43d127d63a4c5725d8
goldeagle-pro <72759515+goldeagle-pro@users.noreply.github.com>
2020-10-17 02:45:28 +0900

    select num

--- js/number.js
+++
@@ -1,8 +0,0 @@
-let five = document.getElementById('five')
-
-document.createElement('option')
-for(let i = 0; i <= 5; i++) {
-    let option = Document.createElement('option');
-    option.setAttribute('value', i);
-    five.appendChild(option);
-};
