d3df6ab615968cfbb34bf263d249ce5e92e9c208
goldeagle-pro <72759515+goldeagle-pro@users.noreply.github.com>
2020-10-23 00:15:35 +0900

    Update TH2dtsc.js

---
+++ js/TH2dtsc.js
@@ -0,0 +1,63 @@
+function DT2skill() {
+	var SCa = document.getElementById('SCa').selectedIndex;
+	var SCb = document.getElementById('SCb').selectedIndex;
+	var SCc = document.getElementById('SCc').selectedIndex;
+	var SCd = document.getElementById('SCd').selectedIndex;
+	var SCe = document.getElementById('SCe').selectedIndex;
+	var SCf = document.getElementById('SCf').selectedIndex;
+	var SCg = document.getElementById('SCg').selectedIndex;
+	var SCh = document.getElementById('SCh').selectedIndex;
+
+	var ARa = document.getElementById('ARa').selectedIndex;
+	var ARb = document.getElementById('ARb').selectedIndex;
+	var ARc = document.getElementById('ARc').selectedIndex;
+	var ARd = document.getElementById('ARd').selectedIndex;
+	var ARe = document.getElementById('ARe').selectedIndex;
+	var ARf = document.getElementById('ARf').selectedIndex;
+
+	var ASa = document.getElementById('ASa').selectedIndex;
+	var ASb = document.getElementById('ASb').selectedIndex;
+	var ASc = document.getElementById('ASc').selectedIndex;
+	var ASd = document.getElementById('ASd').selectedIndex;
+	var ASe = document.getElementById('ASe').selectedIndex;
+	var ASf = document.getElementById('ASf').selectedIndex;
+
+	var SNa = document.getElementById('SNa').selectedIndex;
+	var SNb = document.getElementById('SNb').selectedIndex;
+	var SNc = document.getElementById('SNc').selectedIndex;
+	var SNd = document.getElementById('SNd').selectedIndex;
+	var SNe = document.getElementById('SNe').selectedIndex;
+	var SNf = document.getElementById('SNf').selectedIndex;
+	var SNg = document.getElementById('SNg').selectedIndex;
+
+	var THa = document.getElementById('THa').selectedIndex;
+	var THb = document.getElementById('THb').selectedIndex;
+	var THc = document.getElementById('THc').selectedIndex;
+	var THd = document.getElementById('THd').selectedIndex;
+	var THe = document.getElementById('THe').selectedIndex;
+	var THf = document.getElementById('THf').selectedIndex;
+	var THg = document.getElementById('THg').selectedIndex;
+	var THh = document.getElementById('THh').selectedIndex;
+	var THi = document.getElementById('THi').selectedIndex;
+	var THj = document.getElementById('THj').selectedIndex;
+	var THk = document.getElementById('THk').selectedIndex;
+	var THl = document.getElementById('THl').selectedIndex;
+
+	var KNa = document.getElementById('KNa').selectedIndex;
+	var KNb = document.getElementById('KNb').selectedIndex;
+	var KNc = document.getElementById('KNc').selectedIndex;
+	var KNd = document.getElementById('KNd').selectedIndex;
+	var KNe = document.getElementById('KNe').selectedIndex;
+	var KNf = document.getElementById('KNf').selectedIndex;
+	var KNg = document.getElementById('KNg').selectedIndex;
+	var KNh = document.getElementById('KNh').selectedIndex;
+	var KNi = document.getElementById('KNi').selectedIndex;
+	var KNj = document.getElementById('KNj').selectedIndex;
+	var KNk = document.getElementById('KNk').selectedIndex;
+	var KNl = document.getElementById('KNl').selectedIndex;
+	var KNm = document.getElementById('KNm').selectedIndex;
+
+	var skillScA = document.getElementById('skillScA');
+		textScA = SCa*(SCa+1);
+			skillScA.innerHTML = textScA;
+}
