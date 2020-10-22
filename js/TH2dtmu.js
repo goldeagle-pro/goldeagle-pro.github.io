1799d327f25acd385b1c78b6f883350a8d708090
goldeagle-pro <72759515+goldeagle-pro@users.noreply.github.com>
2020-10-22 10:46:09 +0900

    update th2mu

---
+++ js/TH2dtmu.js
@@ -0,0 +1,99 @@
+function DT2skill() {
+	var MUa = document.getElementById('MUa').selectedIndex;
+	var MUb = document.getElementById('MUb').selectedIndex;
+	var MUc = document.getElementById('MUc').selectedIndex;
+	var MUd = document.getElementById('MUd').selectedIndex;
+	var MUe = document.getElementById('MUe').selectedIndex;
+	var MUf = document.getElementById('MUf').selectedIndex;
+	var MUg = document.getElementById('MUg').selectedIndex;
+
+	var SOa = document.getElementById('SOa').selectedIndex;
+	var SOb = document.getElementById('SOb').selectedIndex;
+	var SOc = document.getElementById('SOc').selectedIndex;
+	var SOd = document.getElementById('SOd').selectedIndex;
+	var SOe = document.getElementById('SOe').selectedIndex;
+	var SOf = document.getElementById('SOf').selectedIndex;
+	var SOg = document.getElementById('SOg').selectedIndex;
+	var SOh = document.getElementById('SOh').selectedIndex;
+
+	var ENa = document.getElementById('ENa').selectedIndex;
+	var ENb = document.getElementById('ENb').selectedIndex;
+	var ENc = document.getElementById('ENc').selectedIndex;
+	var ENd = document.getElementById('ENd').selectedIndex;
+	var ENe = document.getElementById('ENe').selectedIndex;
+	var ENf = document.getElementById('ENf').selectedIndex;
+	var ENg = document.getElementById('ENg').selectedIndex;
+	var ENh = document.getElementById('ENh').selectedIndex;
+	var ENi = document.getElementById('ENi').selectedIndex;
+	var ENj = document.getElementById('ENj').selectedIndex;
+	var ENk = document.getElementById('ENk').selectedIndex;
+	var ENl = document.getElementById('ENl').selectedIndex;
+	var ENm = document.getElementById('ENm').selectedIndex;
+	var ENn = document.getElementById('ENn').selectedIndex;
+	var ENo = document.getElementById('ENo').selectedIndex;
+	var ENp = document.getElementById('ENp').selectedIndex;
+	var ENq = document.getElementById('ENq').selectedIndex;
+	var ENr = document.getElementById('ENr').selectedIndex;
+
+	var PRa = document.getElementById('PRa').selectedIndex;
+	var PRb = document.getElementById('PRb').selectedIndex;
+	var PRc = document.getElementById('PRc').selectedIndex;
+	var PRd = document.getElementById('PRd').selectedIndex;
+	var PRe = document.getElementById('PRe').selectedIndex;
+	var PRf = document.getElementById('PRf').selectedIndex;
+	var PRg = document.getElementById('PRg').selectedIndex;
+	var PRh = document.getElementById('PRh').selectedIndex;
+	var PRi = document.getElementById('PRi').selectedIndex;
+	var PRj = document.getElementById('PRj').selectedIndex;
+	var PRk = document.getElementById('PRk').selectedIndex;
+	var PRl = document.getElementById('PRl').selectedIndex;
+	var PRm = document.getElementById('PRm').selectedIndex;
+
+	var WTa = document.getElementById('WTa').selectedIndex;
+	var WTb = document.getElementById('WTb').selectedIndex;
+	var WTc = document.getElementById('WTc').selectedIndex;
+	var WTd = document.getElementById('WTd').selectedIndex;
+	var WTe = document.getElementById('WTe').selectedIndex;
+	var WTf = document.getElementById('WTf').selectedIndex;
+	var WTg = document.getElementById('WTg').selectedIndex;
+	var WTh = document.getElementById('WTh').selectedIndex;
+	var WTi = document.getElementById('WTi').selectedIndex;
+	var WTj = document.getElementById('WTj').selectedIndex;
+
+	var MPa = document.getElementById('MPa').selectedIndex;
+	var MPb = document.getElementById('MPb').selectedIndex;
+	var MPc = document.getElementById('MPc').selectedIndex;
+	var MPd = document.getElementById('MPd').selectedIndex;
+	var MPe = document.getElementById('MPe').selectedIndex;
+	var MPf = document.getElementById('MPf').selectedIndex;
+	var MPg = document.getElementById('MPg').selectedIndex;
+	var MPh = document.getElementById('MPh').selectedIndex;
+	var MPi = document.getElementById('MPi').selectedIndex;
+	var MPj = document.getElementById('MPj').selectedIndex;
+
+	var SGa = document.getElementById('SGa').selectedIndex;
+	var SGb = document.getElementById('SGb').selectedIndex;
+	var SGc = document.getElementById('SGc').selectedIndex;
+	var SGd = document.getElementById('SGd').selectedIndex;
+	var SGe = document.getElementById('SGe').selectedIndex;
+	var SGf = document.getElementById('SGf').selectedIndex;
+	var SGg = document.getElementById('SGg').selectedIndex;
+	var SGh = document.getElementById('SGh').selectedIndex;
+	var SGi = document.getElementById('SGi').selectedIndex;
+	var SGj = document.getElementById('SGj').selectedIndex;
+	var SGk = document.getElementById('SGk').selectedIndex;
+	var SGl = document.getElementById('SGl').selectedIndex;
+	var SGm = document.getElementById('SGm').selectedIndex;
+
+	var BSa = document.getElementById('BSa').selectedIndex;
+	var BSb = document.getElementById('BSb').selectedIndex;
+	var BSc = document.getElementById('BSc').selectedIndex;
+	var BSd = document.getElementById('BSd').selectedIndex;
+	var BSe = document.getElementById('BSe').selectedIndex;
+	var BSf = document.getElementById('BSf').selectedIndex;
+	var BSg = document.getElementById('BSg').selectedIndex;
+	var BSh = document.getElementById('BSh').selectedIndex;
+	var BSi = document.getElementById('BSi').selectedIndex;
+	var BSj = document.getElementById('BSj').selectedIndex;
+
+}
