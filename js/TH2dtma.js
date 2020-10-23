cc49dd3c58493da67ba6952674c22245647d0a2a
goldeagle-pro <72759515+goldeagle-pro@users.noreply.github.com>
2020-10-23 13:08:04 +0900

    update th2 ma

---
+++ js/TH2dtma.js
@@ -0,0 +1,83 @@
+function DT2skill() {
+	var MAa = document.getElementById('MAa').selectedIndex;
+	var MAb = document.getElementById('MAb').selectedIndex;
+	var MAc = document.getElementById('MAc').selectedIndex;
+	var MAd = document.getElementById('MAd').selectedIndex;
+	var MAe = document.getElementById('MAe').selectedIndex;
+	var MAf = document.getElementById('MAf').selectedIndex;
+	var MAg = document.getElementById('MAg').selectedIndex;
+
+	var BDa = document.getElementById('BDa').selectedIndex;
+	var BDb = document.getElementById('BDb').selectedIndex;
+	var BDc = document.getElementById('BDc').selectedIndex;
+	var BDd = document.getElementById('BDd').selectedIndex;
+	var BDe = document.getElementById('BDe').selectedIndex;
+	var BDf = document.getElementById('BDf').selectedIndex;
+
+	var DNa = document.getElementById('DNa').selectedIndex;
+	var DNb = document.getElementById('DNb').selectedIndex;
+	var DNc = document.getElementById('DNc').selectedIndex;
+	var DNd = document.getElementById('DNd').selectedIndex;
+	var DNe = document.getElementById('DNe').selectedIndex;
+
+	var UHa = document.getElementById('UHa').selectedIndex;
+	var UHb = document.getElementById('UHb').selectedIndex;
+	var UHc = document.getElementById('UHc').selectedIndex;
+	var UHd = document.getElementById('UHd').selectedIndex;
+	var UHe = document.getElementById('UHe').selectedIndex;
+	var UHf = document.getElementById('UHf').selectedIndex;
+	var UHg = document.getElementById('UHg').selectedIndex;
+	var UHh = document.getElementById('UHh').selectedIndex;
+	var UHi = document.getElementById('UHi').selectedIndex;
+
+	var JKa = document.getElementById('JKa').selectedIndex;
+	var JKb = document.getElementById('JKb').selectedIndex;
+	var JKc = document.getElementById('JKc').selectedIndex;
+	var JKd = document.getElementById('JKd').selectedIndex;
+	var JKe = document.getElementById('JKe').selectedIndex;
+	var JKf = document.getElementById('JKf').selectedIndex;
+	var JKg = document.getElementById('JKg').selectedIndex;
+	var JKh = document.getElementById('JKh').selectedIndex;
+	var JKi = document.getElementById('JKi').selectedIndex;
+	var JKj = document.getElementById('JKj').selectedIndex;
+	var JKk = document.getElementById('JKk').selectedIndex;
+	var JKl = document.getElementById('JKl').selectedIndex;
+	var JKm = document.getElementById('JKm').selectedIndex;
+	var JKn = document.getElementById('JKn').selectedIndex;
+	var JKo = document.getElementById('JKo').selectedIndex;
+	var JKp = document.getElementById('JKp').selectedIndex;
+	var JKq = document.getElementById('JKq').selectedIndex;
+	var JKr = document.getElementById('JKr').selectedIndex;
+	var JKs = document.getElementById('JKs').selectedIndex;
+	var JKt = document.getElementById('JKt').selectedIndex;
+	var JKu = document.getElementById('JKu').selectedIndex;
+	var JKv = document.getElementById('JKv').selectedIndex;
+	var JKw = document.getElementById('JKw').selectedIndex;
+	var JKx = document.getElementById('JKx').selectedIndex;
+	var JKy = document.getElementById('JKy').selectedIndex;
+	var JKz = document.getElementById('JKz').selectedIndex;
+	var JK01 = document.getElementById('JK01').selectedIndex;
+	var JK02 = document.getElementById('JK02').selectedIndex;
+	var JK03 = document.getElementById('JK03').selectedIndex;
+	var JK04 = document.getElementById('JK04').selectedIndex;
+	var JK05 = document.getElementById('JK05').selectedIndex;
+	var JK06 = document.getElementById('JK06').selectedIndex;
+	var JK07 = document.getElementById('JK07').selectedIndex;
+	var JK08 = document.getElementById('JK08').selectedIndex;
+	var JK09 = document.getElementById('JK09').selectedIndex;
+	var JK10 = document.getElementById('JK10').selectedIndex;
+	var JK11 = document.getElementById('JK11').selectedIndex;
+
+	var MIa = document.getElementById('MIa').selectedIndex;
+	var MIb = document.getElementById('MIb').selectedIndex;
+	var MIc = document.getElementById('MIc').selectedIndex;
+	var MId = document.getElementById('MId').selectedIndex;
+	var MIe = document.getElementById('MIe').selectedIndex;
+	var MIf = document.getElementById('MIf').selectedIndex;
+	var MIg = document.getElementById('MIg').selectedIndex;
+	var MIh = document.getElementById('MIh').selectedIndex;
+	var MIi = document.getElementById('MIi').selectedIndex;
+
+	var skillMaA = document.getElementById('skillMaA');
+	
+}
