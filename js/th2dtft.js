23758922388ccd277e751ab6019ca39612701c89
goldeagle-pro <72759515+goldeagle-pro@users.noreply.github.com>
2020-10-28 23:54:26 +0900

    update th2 ft

--- js/th2dtft.js
+++ js/th2dtft.js
@@ -12,6 +12,18 @@ function DTskill() {
 	var PL02 = document.getElementById('PL02').selectedIndex;
 	var PL03 = document.getElementById('PL03').selectedIndex;
 	var PL04 = document.getElementById('PL04').selectedIndex;
+	var PL05 = document.getElementById('PL05').selectedIndex;
+	var PL06 = document.getElementById('PL06').selectedIndex;
+	var PL07 = document.getElementById('PL07').selectedIndex;
+
+	var DK01 = document.getElementById('DK01').selectedIndex;
+	var DK02 = document.getElementById('DK02').selectedIndex;
+	var DK03 = document.getElementById('DK03').selectedIndex;
+	var DK04 = document.getElementById('DK04').selectedIndex;
+	var DK05 = document.getElementById('DK05').selectedIndex;
+	var DK06 = document.getElementById('DK06').selectedIndex;
+	var DK07 = document.getElementById('DK07').selectedIndex;
+	var DK08 = document.getElementById('DK08').selectedIndex;
 
 	var skillFT01 = document.getElementById('skillFT01');
 		textFT01 = FT01*(FT01+1);
@@ -50,4 +62,56 @@ function DTskill() {
 	var skillPL04 = document.getElementById('skillPL04');
 		textPL04 = PL04*(PL04+1);
 			skillPL04.innerHTML = textPL04;
+	var skillPL05 = document.getElementById('skillPL05');
+		textPL05 = PL05*(PL05+1)/2;
+			skillPL05.innerHTML = textPL05;
+	var skillPL06 = document.getElementById('skillPL06');
+		textPL06 = PL06*(PL06+1);
+			skillPL06.innerHTML = textPL06;
+	var skillPL07 = document.getElementById('skillPL07');
+		textPL07 = PL07*(PL07+1);
+			skillPL07.innerHTML = textPL07;
+	
+	var skillDK01 = document.getElementById('skillDK01');
+		textDK01 = DK01*(DK01+1);
+			skillDK01.innerHTML = textDK01;
+	var skillDK02 = document.getElementById('skillDK02');
+		textDK02 = DK02*(DK02+1)/2;
+			skillDK02.innerHTML = textDK02;
+	var skillDK03 = document.getElementById('skillDK03');
+		textDK03 = DK03*(DK03+1)/2;
+			skillDK03.innerHTML = textDK03;
+	var skillDK04 = document.getElementById('skillDK04');
+		textDK04 = DK04*(DK04+1)/2;
+			skillDK04.innerHTML = textDK04;
+	var skillDK05 = document.getElementById('skillDK05');
+		textDK05 = DK05*(DK05+1);
+			skillDK05.innerHTML = textDK05;
+	var skillDK06 = document.getElementById('skillDK06');
+		textDK06 = DK06*(DK06+1);
+			skillDK06.innerHTML = textDK06;
+	var skillDK07 = document.getElementById('skillDK07');
+		textDK07 = DK07*(DK07+1)/2;
+			skillDK07.innerHTML = textDK07;
+	var skillDK08 = document.getElementById('skillDK08');
+		textDK08 = DK08*(DK08+1);
+			skillDK08.innerHTML = textDK08;
+}
+
+function culcSP() {
+	var skillSPs = [Number(document.getElementById('skillFT01').textContent),
+			Number(document.getElementById('skillFT02').textContent),
+			Number(document.getElementById('skillFT03').textContent),
+			Number(document.getElementById('skillFT04').textContent),
+			Number(document.getElementById('skillFT05').textContent),
+			Number(document.getElementById('skillFT06').textContent),
+			Number(document.getElementById('skillFT07').textContent),
+			Number(document.getElementById('skillFT08').textContent)
+];
+
+	sumSP = skillSPs.reduce(function(a, x){
+		return a + x;
+	},0);
+
+	culcSP.innerHTML = sumSP;
 }
