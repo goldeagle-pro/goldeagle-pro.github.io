a3dd09c57f9e360371e38f6f8feb6f5711b79e2b
goldeagle-pro <72759515+goldeagle-pro@users.noreply.github.com>
2020-10-28 12:43:10 +0900

    update th2 fighter

---
+++ js/TH2dtcopy.js
@@ -0,0 +1,42 @@
+window.onload = Job;
+
+function Job(){
+	JobA();
+	JobB();
+}
+
+function JobA(){
+	changeDispA(1);
+	document.getElementById('changeSelectA').onchange =function(e){
+		changeDispA(0);
+		document.getElementById(this.options[this.selectedIndex].value).style.display = "block";
+	}
+}
+
+function changeDispA(start){
+	var items = document.getElementById('selectDispA').children;
+	for(var i=start; i<items.length; i++){
+		items[i].style.display = "none";
+	}
+}
+
+function JobB(){
+	changeDispB(1);
+	document.getElementById('changeSelectB').onchange =function(e){
+		changeDispB(0);
+		document.getElementById(this.options[this.selectedIndex].value).style.display = "block";
+	}
+}
+
+function changeDispB(start){
+	var items = document.getElementById('selectDispB').children;
+	for(var i=start; i<items.length; i++){
+		items[i].style.display = "none";
+	}
+}
+
+function totalLv() {
+	var nowLv = document.getElementById('totalLv');
+	nowLv.innerHTML = document.getElementById('curLv').value;
+}
+
