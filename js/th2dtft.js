d7d0507e0d64452e09603b4aa5f180de1f80a4b2
goldeagle-pro <72759515+goldeagle-pro@users.noreply.github.com>
2020-10-28 18:05:13 +0900

    update th2 ft

--- js/th2dtft.js
+++ js/th2dtft.js
@@ -4,6 +4,9 @@ function DTskill() {
 	var FT03 = document.getElementById('FT03').selectedIndex;
 	var FT04 = document.getElementById('FT04').selectedIndex;
 	var FT05 = document.getElementById('FT05').selectedIndex;
+	var FT06 = document.getElementById('FT06').selectedIndex;
+	var FT07 = document.getElementById('FT07').selectedIndex;
+	var FT08 = document.getElementById('FT08').selectedIndex;
 
 	var skillFT01 = document.getElementById('skillFT01');
 		textFT01 = FT01*(FT01+1);
@@ -20,4 +23,13 @@ function DTskill() {
 	var skillFT05 = document.getElementById('skillFT05');
 		textFT05 = FT05*(FT05+1)/2;
 			skillFT05.innerHTML = textFT05;
+	var skillFT06 = document.getElementById('skillFT06');
+		textFT06 = FT06*(FT06+1)/2;
+			skillFT06.innerHTML = textFT06;
+	var skillFT07 = document.getElementById('skillFT07');
+		textFT07 = FT07*(FT07+1);
+			skillFT07.innerHTML = textFT07;
+	var skillFT08 = document.getElementById('skillFT08');
+		textFT08 = FT08*(FT08+1);
+			skillFT08.innerHTML = textFT08;
 }
