38154b47da4b3b4af309be51d2894feb5463e6c5
goldeagle-pro <72759515+goldeagle-pro@users.noreply.github.com>
2020-10-29 17:36:54 +0900

    update th2 ft

--- js/TH2dtcopy.js
+++ js/TH2dtcopy.js
@@ -1,36 +1,21 @@
-window.onload = Job;
+window.onload = job();
 
-function Job(){
-	JobA();
-	JobB();
+job() {
+	jobA();
 }
 
-function JobA(){
+function jobA() {
 	changeDispA(1);
-	document.getElementById('changeSelectA').onchange =function(e){
-		changeDispA(0);
-		document.getElementById(this.options[this.selectedIndex].value).style.display = "block";
+	document.getElementById('changeSelectA').onchange = function(e) {
+	changeDispA(0);
+	document.getElementById(this.options[this.selectedIndex].value).style.display = "block";
 	}
 }
 
-function changeDispA(start){
+
+function changeDispA(x) {
 	var items = document.getElementById('selectDispA').children;
-	for(var i=start; i<items.length; i++){
-		items[i].style.display = "none";
-	}
-}
-
-function JobB(){
-	changeDispB(1);
-	document.getElementById('changeSelectB').onchange =function(e){
-		changeDispB(0);
-		document.getElementById(this.options[this.selectedIndex].value).style.display = "block";
-	}
-}
-
-function changeDispB(start){
-	var items = document.getElementById('selectDispB').children;
-	for(var i=start; i<items.length; i++){
+	for(i = x; i < items.length; i++) {
 		items[i].style.display = "none";
 	}
 }
