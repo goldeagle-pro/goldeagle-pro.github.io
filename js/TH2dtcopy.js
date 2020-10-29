f14c6586003370536c7614ba135179a222d8ee93
goldeagle-pro <72759515+goldeagle-pro@users.noreply.github.com>
2020-10-29 23:54:07 +0900

    update th2 ft

--- js/TH2dtcopy.js
+++ js/TH2dtcopy.js
@@ -2,6 +2,7 @@ window.onload = job();
 
 job() {
 	jobA();
+	jobB();
 }
 
 function jobA() {
@@ -19,6 +20,22 @@ function changeDispA(x) {
 		items[i].style.display = "none";
 	}
 }
+
+function jobB() {
+	changeDispB(1);
+	document.getElementById('changeSelectB').onchange = function(e) {
+		changeDispB(0);
+		document.getElementById(this.options[this.selectedIndex].value).style.display = "block";
+	}
+}
+
+function changeDispB(x) {
+	var items = document.getElementById('skillDispB').children;
+	for(i=x; i<items.length; i++) {
+		items[i].style.display ="none";
+	}
+}
+
 
 function totalLv() {
 	var nowLv = document.getElementById('totalLv');
