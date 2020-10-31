df536d9a7331275c440ee48d94ca090dca83bf1e
goldeagle-pro <72759515+goldeagle-pro@users.noreply.github.com>
2020-10-31 17:47:56 +0900

    update

--- js/th2dt.js
+++ js/th2dt.js
@@ -1,84 +1,79 @@
-window.onload = Job;
+window.onload = job();
 
-function Job(){
-	JobA();
-	JobB();
+function job() {
+	jobA();
+	jobB();
 }
 
-function JobA(){
+function jobA() {
 	changeDispA(1);
-	document.getElementById('changeSelectA').onchange =function(e){
-		changeDispA(0);
-		document.getElementById(this.options[this.selectedIndex].value).style.display = "block";
+	document.getElementById('changeSelectA').onchange = function(e) {
+	changeDispA(0);
+	document.getElementById(this.options[this.selectedIndex].value).style.display = "block";
 	}
 }
 
-function changeDispA(start){
+
+function changeDispA(x) {
 	var items = document.getElementById('selectDispA').children;
-	for(var i=start; i<items.length; i++){
+	for(i = x; i < items.length; i++) {
 		items[i].style.display = "none";
 	}
 }
 
-function JobB(){
+function jobB() {
 	changeDispB(1);
-	document.getElementById('changeSelectB').onchange =function(e){
+	document.getElementById('changeSelectB').onchange = function(e) {
 		changeDispB(0);
 		document.getElementById(this.options[this.selectedIndex].value).style.display = "block";
 	}
 }
 
-function changeDispB(start){
+function changeDispB(x) {
 	var items = document.getElementById('selectDispB').children;
-	for(var i=start; i<items.length; i++){
-		items[i].style.display = "none";
+	for(i=x; i<items.length; i++) {
+		items[i].style.display ="none";
 	}
 }
 
-function remSP(){
-  var curLv =document.getElementById('curLv').value;
-   var remSP = document.getElementById('remSP');
 
-var arraySPs = [3,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
-	17,19,21,23,25,27,29,31,33,35,37,39,41,44,47,50,
-	53,56,59,63,67,71,75,79,83,87,91,95,99,103,108,
-	113,118,123,128,134,140,146,152,158,165,172,179,
-	186,193,200,208,216,224,232,240,249,258,267,276,
-	286,296,306,316,326,337,348,359,370,382,394,406,
-	418,431,444,457,470,484,498,512,526,541,556,571,
-	586,602,618,634,651,668];
-
-	var arraySP = arraySPs[Number(curLv)];
-
-var sumRemSP = Number(arraySP)
-				+Number(document.getElementById('skillDrug').value)
-				-Number(document.getElementById('culcSP').innerHTML);
-				
-remSP.innerHTML = sumRemSP;
-
+function totalLv() {
+	document.getElementById('totalLv').innerHTML = document.getElementById('currentLv').value;
 }
 
- function clearAll(){
-   document.skillSim.reset();
-   
-   var culcSP = document.getElementById('culcSP');
-   culcSP.innerHTML = 0;
-
-   var remSP = document.getElementById('remSP')
-   remSP.innerHTML = 3;
- } 
-
-function deleteAll() {
-	var result = confirm('セーブデータを全消去してよろしいですか？');
+function remainSP() {
+	var arraySPs = [3,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
+		17,19,21,23,25,27,29,31,33,35,37,39,41,44,47,50,
+		53,56,59,63,67,71,75,79,83,87,91,95,99,103,108,
+		113,118,123,128,134,140,146,152,158,165,172,179,
+		186,193,200,208,216,224,232,240,249,258,267,276,
+		286,296,306,316,326,337,348,359,370,382,394,406,
+		418,431,444,457,470,484,498,512,526,541,556,571,
+		586,602,618,634,651,668];
 	
-if (result) {
-  var saveNum = document.getElementById('saveDataNum')
-	saveNum.options[saveNum.selectedIndex].textContent =saveNum.selectedIndex + ":"+"no data";
-  
-	localStorage.clear();
-  document.LvData.reset();
+	currentLv = document.getElementById('currentLv');
 
+	arraySP = arraySPs[currentLv.value];
 
+	sumRemainSP = Number(arraySP) 
+			+ Number(document.getElementById('skillDrug').value)
+			- Number(document.getElementById('culcSP').innerHTML);
+	
+	document.getElementById('remainSP').innerHTML = sumRemainSP;
 }
+
+function remove() {
+	localStorage.removeItem( document.getElementById('saveDataNum').selectedIndex);
+
+	document.skillSim.reset();
+	document.saveData.reset();
+}
+
+function clearAll() {
+	document.skillSim.reset();
+	document.getElementById('saveDataName').value = "";
+	document.getElementById('skillDrug').value = "";
+	document.getElementById('currentLv').value = 1;
+	document.getElementById('totalLv').innerHTML = document.getElementById('currentLv').value;
 }
 
