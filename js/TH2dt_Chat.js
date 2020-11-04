18d58613d3f2e3f4e8cc2b58ed3b51cb7709a78b
goldeagle-pro <72759515+goldeagle-pro@users.noreply.github.com>
2020-11-04 21:57:49 +0900

    update th2 chat

--- js/TH2dt_Chat.js
+++ js/TH2dt_Chat.js
@@ -1,9 +1,11 @@
+window.onload = load();
+
 function changeItem(obj) {
 	
 	if(obj.value == 0) {
 		obj.style.color = "red";
 	} else {
-		obj.style.color = "gray";
+		obj.style.color ="gray"
 	}
 }
 
@@ -14,4 +16,31 @@ function changeLine(obj) {
 	} else {
 		obj.style.textDecoration ="line-through"
 	}
+}
+
+function save() {
+	var chatArrays = [];
+
+	for(var i=0; i<108; i++) {
+		var chatData = document.getElementById('chat-'+i).value;
+
+		chatArrays.push(chatData);
+	}
+
+	localStorage.setItem('chatKey',JSON.stringify(chatArrays));
+}
+
+function load() {
+	chatArrays = JSON.parse(localStorage.getItem('chatKey'));
+
+	for(var i=0; i<108; i++) {
+		document.getElementById('chat-'+i).value = chatArrays[i];
+		changeItem(document.getElementById('chat-'+i));
+		changeLine(document.getElementById('chat-'+i));
+	}
+}
+
+function remove() {
+	localStorage.removeItem('chatKey');
+	document.chat.reset();
 }
