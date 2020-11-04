7c59563022ea07417d096ea76e6196754d9d97c5
goldeagle-pro <72759515+goldeagle-pro@users.noreply.github.com>
2020-11-04 15:12:54 +0900

    update th2 chat

---
+++ js/TH2dt_Chat.js
@@ -0,0 +1,17 @@
+function changeItem(obj) {
+	
+	if(obj.value == 0) {
+		obj.style.color = "red";
+	} else {
+		obj.style.color = "gray";
+	}
+};
+
+function changeLine(obj) {
+	
+	if(obj.value == 0) {
+		obj.style.textDecoration = "none";
+	} else {
+		obj.style.textDecoration ="line-through"
+	}
+};
