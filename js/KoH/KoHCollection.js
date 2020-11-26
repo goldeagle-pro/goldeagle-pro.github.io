1b9fee370fd6a188ae360389ff3cb2ba1047a61f
goldeagle-pro <72759515+goldeagle-pro@users.noreply.github.com>
2020-11-26 18:50:33 +0900

    update

--- js/KoH/KoHCollection.js
+++ js/KoH/KoHCollection.js
@@ -20,18 +20,73 @@ function changeAwakening(obj) {
 
 function save() {
 	var obtArrays = [];
-	for(i=0; i<3; i++) {
+	for(i=0; i<186; i++) {
 		var obtData = document.getElementById("obt-"+ i).value;
 		obtArrays.push(obtData);
 	}
 	localStorage.setItem("obtKey",JSON.stringify(obtArrays));
+
+	var awakeArrays = [];
+	for(var i=0; i<186; i++) {
+		var awakeData = document.getElementById('awake-'+i).value;
+		awakeArrays.push(awakeData);
+	}
+	localStorage.setItem("awakeKey",JSON.stringify(awakeArrays));
+
+	var lvArrays = [];
+	for(var i=0; i<186; i++) {
+		var lvData = document.getElementById("Lv-"+i).value;
+		lvArrays.push(lvData);
+	}
+	localStorage.setItem("lvKey",JSON.stringify(lvArrays));
+
+	var skillLvArrays = [];
+	for(var i=0; i<202; i++) {
+		var skillLvData = document.getElementById("sLv-"+i).value;
+		skillLvArrays.push(skillLvData);
+	}
+	localStorage.setItem("skillLvKey",JSON.stringify(skillLvArrays));
 }
 
 function load() {
 	var obtArrays = JSON.parse(localStorage.getItem("obtKey"));
-	for(i=0; i<3; i++) {
+	for(i=0; i<186; i++) {
 		document.getElementById("obt-"+ i).value = obtArrays[i];
 		changeObtain(document.getElementById("obt-"+i));
 	}
+
+	var awakeArrays = JSON.parse(localStorage.getItem("awakeKey"));
+	for(var i=0; i<186; i++) {
+		document.getElementById("awake-"+i).value = awakeArrays[i];
+		changeAwakening(document.getElementById("awake-"+i));
+	}
 	
+	var lvArrays = JSON.parse(localStorage.getItem("lvKey"));
+	for(var i=0; i<186; i++) {
+		document.getElementById("Lv-"+i).value = lvArrays[i];
+	}
+
+	var skillLvArrays = JSON.parse(localStorage.getItem("skillLvKey"));
+	for(var i=0; i<202; i++) {
+		document.getElementById("sLv-"+i).value =skillLvArrays[i];
+	}
 }
+
+jQuery(function() {
+	var pagetop = $('#page_top');   
+	pagetop.hide();
+	$(window).scroll(function () {
+	    if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+		pagetop.fadeIn();
+	    } else {
+		pagetop.fadeOut();
+	    }
+	});
+	pagetop.click(function () {
+	    $('body,html').animate({
+		scrollTop: 0
+	    }, 500); //0.5秒かけてトップへ移動
+	    return false;
+	});
+    });
+    
