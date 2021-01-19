f5ae67480c16f5a442ebba60984787cdbc5c681d
goldeagle-pro <72759515+goldeagle-pro@users.noreply.github.com>
2021-01-19 23:58:55 +0900

    Update Bots.js

--- GAS/Bots/Bots.js
+++ GAS/Bots/Bots.js
@@ -1,5 +1,16 @@
 function myFunction() {
 	const sheet = SpreadsheetApp.getActiveSheet();
-	const range = sheet.getRange('A3:C3');
-	console.log(range.getValues());
+	const lastRow = sheet.getLastRow();
+
+	for(let i = 2; i <= lastRow; i++) {
+		if(!sheet.getRange(i, 4).getValue()) {
+			console.log(sheet.getRange(i, 1).getValue());
+			sheet.getRange(i, 4).setValue(true);
+		
+			if(i >= lastRow) {
+				sheet.getRange(2, 4, lastRow - 1).getA1Notation();
+			}
+			break;	
+		}
+	}
 }
