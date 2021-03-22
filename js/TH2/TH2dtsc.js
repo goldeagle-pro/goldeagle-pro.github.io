510ed0d3def7f0e901462b288f11daa67b618a5a
goldeagle-pro <nabe960free@gmail.com>
2021-03-23 05:42:44 +0900

    Update TH2dtsc.js

--- js/TH2/TH2dtsc.js
+++ js/TH2/TH2dtsc.js
@@ -4,7 +4,7 @@ function loading() {
 	var wave = JSON.parse(localStorage.getItem('startKey'));
 	
 	for(var i=0; i<wave.length; i++) {
-		document.getElementById('saveDataNumScout').options[i].textContent = wave[i]; 
+		document.getElementById('saveDataNum').options[i].textContent = wave[i]; 
 	}
 }
 
@@ -300,7 +300,7 @@ function culcSP() {
 }
 
 function save() {
-	var saveNumber = document.getElementById('saveDataNumScout');
+	var saveNumber = document.getElementById('saveDataNum');
 	var saveWord = document.getElementById('saveDataName').nodeValue;
 
 	selectNumber = saveNumber.options.selectedIndex;
@@ -379,11 +379,11 @@ function save() {
 				document.getElementById('KN13').options.selectedIndex
 	];
 
-	localStorage.setItem(document.getElementById('saveDataNumScout').value,JSON.stringify(saveDataArrays));
+	localStorage.setItem(document.getElementById('saveDataNum	').value,JSON.stringify(saveDataArrays));
 }
 
 function load() {
-	var loadData = JSON.parse(localStorage.getItem(document.getElementById('saveDataNumScout').value));
+	var loadData = JSON.parse(localStorage.getItem(document.getElementById('saveDataNum').value));
 
 	document.getElementById('saveDataName').value = loadData[0];
 	document.getElementById('skillDrug').value = loadData[1];
@@ -456,7 +456,7 @@ function load() {
 }
 
 function remove() {
-	localStorage.removeItem( document.getElementById('saveDataNumScout').selectedIndex);
+	localStorage.removeItem( document.getElementById('saveDataNum').selectedIndex);
 
 	document.skillSim.reset();
 	document.saveData.reset();
